517625ba2c671dd3e2a3d2d4708c0482b1de72f3
Klemen Košir <klemen.kosir@kream.io>
2020-07-19 19:51:20 +0900

    Move private methods to helpers

--- helpers.js
+++ helpers.js
@@ -1,7 +1,73 @@
 "use strict";
 
+const os = require("os");
+const util = require("util");
+
+function write(log, stream, messages, color = null) {
+    const context = {
+        $timestamp: getTimestamp(),
+        $tags: [],
+        $message: "",
+        $output: "",
+        $arguments: [...messages],
+    };
+
+    // If there is more than one argument, treat the first one as the tags.
+    if (messages.length > 1) {
+        const tags = messages.shift();
+        context.$tags = Array.isArray(tags) ? tags : [tags];
+    }
+
+    context.$message = messages.reduce((acc, el) => {
+        let message = el;
+
+        if (el instanceof Error) {
+            message = el.stack;
+        } else if (typeof el === "object") {
+            try {
+                message = JSON.stringify(el, null, 4);
+            } catch (_) {
+                // Handle circular references.
+                message = util.inspect(el);
+            }
+        }
+
+        const separator = el instanceof Error ? os.EOL : " ";
+        return acc + message + separator;
+    }, "").trim();
+
+    const tags = context.$tags.map((t) => `[${t}] `).join("");
+    context.$output = `${context.$timestamp} | ${tags}${context.$message}`;
+
+    executeHooks(log, "beforeWrite", context);
+
+    if (!log.$config.quiet) {
+        if (log.$config.colors && color instanceof Function) {
+            stream(color(context.$output));
+        } else {
+            stream(context.$output);
+        }
+    }
+
+    executeHooks(log, "afterWrite", context);
+
+    for (const [name, fn] of Object.entries(log._$extensions)) {
+        context[name] = (...args) => fn(context, ...args);
+    }
+
+    return context;
+}
+
+function executeHooks(log, event, context) {
+    for (const hook of log._$hooks[event]) {
+        if (hook instanceof Function) {
+            hook(context);
+        }
+    }
+}
+
 function getTimestamp() {
-    const date = new Date(Date.now());
+    const date = new Date();
 
     const year = date.getFullYear();
     const month = pad(date.getMonth() + 1);
@@ -22,5 +88,6 @@ function pad(number) {
 }
 
 module.exports = {
+    write,
     getTimestamp,
 };
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
 
 const chalk = require("chalk");
-const os = require("os");
-const util = require("util");
-
 const helpers = require("./helpers");
 
 class Log {
@@ -69,86 +66,22 @@ class Log {
     }
 
     info(...messages) {
-        return this._write(this.$config.console.info, messages);
+        return helpers.write(this, this.$config.console.info, messages);
     }
 
     success(...messages) {
-        return this._write(this.$config.console.log, messages, chalk.green);
+        return helpers.write(this, this.$config.console.log, messages, chalk.green);
     }
 
     warn(...messages) {
-        return this._write(this.$config.console.warn, messages, chalk.yellow);
+        return helpers.write(this, this.$config.console.warn, messages, chalk.yellow);
     }
 
     error(...messages) {
-        return this._write(this.$config.console.error, messages, chalk.red);
-    }
-
-    _write(stream, messages, color = null) {
-        const context = {
-            $timestamp: helpers.getTimestamp(),
-            $tags: [],
-            $message: "",
-            $output: "",
-            $arguments: [...messages],
-        };
-
-        // If there is more than one argument, treat the first one as the tags.
-        if (messages.length > 1) {
-            const tags = messages.shift();
-            context.$tags = Array.isArray(tags) ? tags : [tags];
-        }
-
-        context.$message = messages.reduce((acc, el) => {
-            let message = el;
-
-            if (el instanceof Error) {
-                message = el.stack;
-            } else if (typeof el === "object") {
-                try {
-                    message = JSON.stringify(el, null, 4);
-                } catch (_) {
-                    // Handle circular references.
-                    message = util.inspect(el);
-                }
-            }
-
-            const separator = el instanceof Error ? os.EOL : " ";
-            return acc + message + separator;
-        }, "").trim();
-
-        const tags = context.$tags.map((t) => `[${t}] `).join("");
-        context.$output = `${context.$timestamp} | ${tags}${context.$message}`;
-
-        this._executeHooks("beforeWrite", context);
-
-        if (!this.$config.quiet) {
-            if (this.$config.colors && color instanceof Function) {
-                stream(color(context.$output));
-            } else {
-                stream(context.$output);
-            }
-        }
-
-        this._executeHooks("afterWrite", context);
-
-        for (const [name, fn] of Object.entries(this._$extensions)) {
-            context[name] = (...args) => fn(context, ...args);
-        }
-
-        return context;
-    }
-
-    _executeHooks(event, context) {
-        for (const hook of this._$hooks[event]) {
-            if (hook instanceof Function) {
-                hook(context);
-            }
-        }
+        return helpers.write(this, this.$config.console.error, messages, chalk.red);
     }
 }
 
 const unklogger = new Log();
-
 module.exports = unklogger;
 module.exports.default = unklogger;
