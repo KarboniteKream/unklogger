6fc1b8fbea3e60d5c6c41af759884599e162d093
Klemen Košir <klemen.kosir@kream.io>
2024-12-30 10:18:29 +0900

    chore: Replace Mocha with `node:test`

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
+import { before, beforeEach, describe, it } from "node:test";
+
 import { assert } from "chai";
 import chalk from "chalk";
 
 import Log from "./index.js";
 import { getTimestamp } from "./helpers.js";
 
-before(function() {
+const STATE = {
+    $console: null,
+    $stream: null,
+    $timestamp: getTimestamp(),
+};
+
+before(() => {
     const mock = (stream) => (text) => {
-        this.$stream = stream;
-        this.$console = text;
+        STATE.$stream = stream;
+        STATE.$console = text;
     };
 
     Log.$config = {
@@ -24,22 +32,22 @@ before(function() {
     Date.now = () => 732652245000;
 });
 
-beforeEach(function() {
-    this.$console = null;
-    this.$stream = null;
-    this.$timestamp = getTimestamp();
+beforeEach(() => {
+    STATE.$console = null;
+    STATE.$stream = null;
+    STATE.$timestamp = getTimestamp();
 });
 
-describe("Helpers", function() {
-    describe("getTimestamp()", function() {
-        it("correctly formats the date", function() {
-            assert.match(this.$timestamp, /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/);
+describe("Helpers", () => {
+    describe("getTimestamp()", () => {
+        it("correctly formats the date", () => {
+            assert.match(STATE.$timestamp, /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/);
         });
     });
 });
 
-describe("Log", function() {
-    it("creates a new instance", function() {
+describe("Log", () => {
+    it("creates a new instance", () => {
         const first = Log.new();
         assert.notEqual(Log.$config, first.$config);
         assert.notEqual(Log.$config.colors, first.$config.colors);
@@ -49,7 +57,7 @@ describe("Log", function() {
         assert.notEqual(first.$config.colors, second.$config.colors);
     });
 
-    it("clones an instance", function() {
+    it("clones an instance", () => {
         const first = Log.new();
         first.$config.colors = false;
         const second = first.clone();
@@ -58,190 +66,190 @@ describe("Log", function() {
         assert.equal(first.$config.colors, second.$config.colors);
     });
 
-    it("correctly formats the output", function() {
+    it("correctly formats the output", () => {
         Log.info("FOO");
-        assert.equal(this.$console, `${this.$timestamp} | FOO`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO`);
     });
 
-    it("is using correct streams", function() {
+    it("is using correct streams", () => {
         Log.info("FOO");
-        assert.equal(this.$stream, "info");
+        assert.equal(STATE.$stream, "info");
 
         Log.success("FOO");
-        assert.equal(this.$stream, "log");
+        assert.equal(STATE.$stream, "log");
 
         Log.warn("FOO");
-        assert.equal(this.$stream, "warn");
+        assert.equal(STATE.$stream, "warn");
 
         Log.error("FOO");
-        assert.equal(this.$stream, "error");
+        assert.equal(STATE.$stream, "error");
     });
 
-    it("is using correct colors", function() {
+    it("is using correct colors", () => {
         const log = Log.clone();
         log.$config.colors = true;
 
         log.info("FOO");
-        assert.equal(this.$console, `${this.$timestamp} | FOO`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO`);
 
         log.success("FOO");
-        assert.equal(this.$console, chalk.green(`${this.$timestamp} | FOO`));
+        assert.equal(STATE.$console, chalk.green(`${STATE.$timestamp} | FOO`));
 
         log.warn("FOO");
-        assert.equal(this.$console, chalk.yellow(`${this.$timestamp} | FOO`));
+        assert.equal(STATE.$console, chalk.yellow(`${STATE.$timestamp} | FOO`));
 
         log.error("FOO");
-        assert.equal(this.$console, chalk.red(`${this.$timestamp} | FOO`));
+        assert.equal(STATE.$console, chalk.red(`${STATE.$timestamp} | FOO`));
     });
 
-    it("handles invalid arguments", function() {
+    it("handles invalid arguments", () => {
         Log.info();
-        assert.equal(this.$console, `${this.$timestamp} | `);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | `);
 
         Log.info(null);
-        assert.equal(this.$console, `${this.$timestamp} | null`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | null`);
 
         Log.info(null, null);
-        assert.equal(this.$console, `${this.$timestamp} | [null] null`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [null] null`);
 
         Log.info(undefined, null);
-        assert.equal(this.$console, `${this.$timestamp} | [undefined] null`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [undefined] null`);
     });
 
-    it("suppresses output with 'quiet'", function() {
+    it("suppresses output with 'quiet'", () => {
         const log = Log.clone();
         log.$config.quiet = true;
 
         log.info("FOO");
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
     });
 });
 
-describe("Tags", function() {
-    it("outputs no tags", function() {
+describe("Tags", () => {
+    it("outputs no tags", () => {
         Log.info([], "FOO");
-        assert.equal(this.$console, `${this.$timestamp} | FOO`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO`);
     });
 
-    it("outputs a single tag", function() {
+    it("outputs a single tag", () => {
         Log.info("FOO", "BAR");
-        assert.equal(this.$console, `${this.$timestamp} | [FOO] BAR`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [FOO] BAR`);
     });
 
-    it("outputs multiple tags", function() {
+    it("outputs multiple tags", () => {
         Log.info(["FOO", "BAR"], "FOO");
-        assert.equal(this.$console, `${this.$timestamp} | [FOO] [BAR] FOO`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [FOO] [BAR] FOO`);
     });
 });
 
-describe("Strings", function() {
-    it("outputs a string", function() {
+describe("Strings", () => {
+    it("outputs a string", () => {
         Log.info("FOO");
-        assert.equal(this.$console, `${this.$timestamp} | FOO`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO`);
     });
 
-    it("outputs multiple strings", function() {
+    it("outputs multiple strings", () => {
         Log.info([], "FOO", "BAR");
-        assert.equal(this.$console, `${this.$timestamp} | FOO BAR`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO BAR`);
     });
 });
 
-describe("Arrays", function() {
-    it("outputs an array", function() {
+describe("Arrays", () => {
+    it("outputs an array", () => {
         Log.info([1, 2, 3]);
-        assert.equal(this.$console, `${this.$timestamp} | [\n    1,\n    2,\n    3\n]`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [\n    1,\n    2,\n    3\n]`);
     });
 });
 
-describe("Objects", function() {
-    it("outputs an object", function() {
+describe("Objects", () => {
+    it("outputs an object", () => {
         Log.info({ foo: "FOO", bar: "BAR" });
-        assert.equal(this.$console, `${this.$timestamp} | {\n    "foo": "FOO",\n    "bar": "BAR"\n}`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | {\n    "foo": "FOO",\n    "bar": "BAR"\n}`);
     });
 
-    it("outputs an object with a circular reference", function() {
+    it("outputs an object with a circular reference", () => {
         const obj = { foo: "FOO", bar: "BAR" };
         obj.obj = obj;
 
         Log.info(obj);
-        assert.oneOf(this.$console, [
-            `${this.$timestamp} | { foo: 'FOO', bar: 'BAR', obj: [Circular] }`,
-            `${this.$timestamp} | <ref *1> { foo: 'FOO', bar: 'BAR', obj: [Circular *1] }`,
+        assert.oneOf(STATE.$console, [
+            `${STATE.$timestamp} | { foo: 'FOO', bar: 'BAR', obj: [Circular] }`,
+            `${STATE.$timestamp} | <ref *1> { foo: 'FOO', bar: 'BAR', obj: [Circular *1] }`,
         ]);
     });
 });
 
-describe("Errors", function() {
-    it("outputs an error", function() {
+describe("Errors", () => {
+    it("outputs an error", () => {
         Log.info(new Error("FOO"));
-        assert.equal(this.$console.substr(0, 37), `${this.$timestamp} | Error: FOO\n    `);
+        assert.equal(STATE.$console.substr(0, 37), `${STATE.$timestamp} | Error: FOO\n    `);
     });
 });
 
-describe("Functions", function() {
-    it("outputs a named function", function() {
+describe("Functions", () => {
+    it("outputs a named function", () => {
         Log.info(function foo() {});
-        assert.equal(this.$console, `${this.$timestamp} | function foo() {}`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | function foo() {}`);
     });
 
-    it("outputs an anonymous function", function() {
+    it("outputs an anonymous function", () => {
         Log.info(function() {});
-        assert.equal(this.$console, `${this.$timestamp} | function() {}`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | function() {}`);
     });
 
-    it("outputs an arrow function", function() {
+    it("outputs an arrow function", () => {
         Log.info(() => {});
-        assert.equal(this.$console, `${this.$timestamp} | () => {}`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | () => {}`);
     });
 });
 
-describe("Other", function() {
-    it("outputs mixed variable types", function() {
+describe("Other", () => {
+    it("outputs mixed variable types", () => {
         Log.info([], "FOO", ["BAR"]);
-        assert.equal(this.$console, `${this.$timestamp} | FOO [\n    "BAR"\n]`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO [\n    "BAR"\n]`);
     });
 
-    it("outputs nested variables", function() {
+    it("outputs nested variables", () => {
         Log.info([], { foo: "FOO", bar: ["BAR"] });
-        assert.equal(this.$console, `${this.$timestamp} | {\n    "foo": "FOO",\n    "bar": [\n        "BAR"\n    ]\n}`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | {\n    "foo": "FOO",\n    "bar": [\n        "BAR"\n    ]\n}`);
     });
 });
 
-describe("Hooks", function() {
-    it("only allow string as a name", function() {
+describe("Hooks", () => {
+    it("only allow string as a name", () => {
         const log = Log.clone();
 
         log.addHook("beforeWrite", () => {});
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
 
         log.addHook(["beforeWrite"], () => {});
-        assert.equal(this.$console, `${this.$timestamp} | [unklogger] Argument 'event' is not a string.`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [unklogger] Argument 'event' is not a string.`);
     });
 
-    it("only allows supported events", function() {
+    it("only allows supported events", () => {
         const log = Log.clone();
 
         log.addHook("beforeWrite", () => {});
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
 
         log.addHook("afterWrite", () => {});
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
 
         log.addHook("FOO", () => {});
-        assert.equal(this.$console, `${this.$timestamp} | [unklogger] Event 'FOO' does not exist.`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [unklogger] Event 'FOO' does not exist.`);
     });
 
-    it("only accepts functions", function() {
+    it("only accepts functions", () => {
         const log = Log.clone();
 
         log.addHook("beforeWrite", () => {});
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
 
         log.addHook("beforeWrite", "FOO");
-        assert.equal(this.$console, `${this.$timestamp} | [unklogger] Argument 'fn' is not a function.`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [unklogger] Argument 'fn' is not a function.`);
     });
 
-    it("executes beforeWrite hooks", function() {
+    it("executes beforeWrite hooks", () => {
         const log = Log.clone();
 
         log.addHook("beforeWrite", (context) => {
@@ -253,43 +261,43 @@ describe("Hooks", function() {
         });
 
         log.info("FOO");
-        assert.equal(this.$console, `${this.$timestamp} | FOO BAR OK`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO BAR OK`);
     });
 
-    it("executes afterWrite hooks", function() {
+    it("executes afterWrite hooks", () => {
         const log = Log.clone();
 
         log.addHook("afterWrite", (context) => {
-            this.$console += " OK";
+            STATE.$console += " OK";
         });
 
         log.info("FOO");
-        assert.equal(this.$console, `${this.$timestamp} | FOO OK`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | FOO OK`);
     });
 });
 
-describe("Context", function() {
-    it("returns correct timestamp", function() {
+describe("Context", () => {
+    it("returns correct timestamp", () => {
         const context = Log.info("FOO", "BAR");
-        assert.equal(context.$timestamp, this.$timestamp);
+        assert.equal(context.$timestamp, STATE.$timestamp);
     });
 
-    it("returns correct tags", function() {
+    it("returns correct tags", () => {
         const context = Log.info(["FOO", "BAR"], "FOO");
         assert.deepEqual(context.$tags, ["FOO", "BAR"]);
     });
 
-    it("returns correct message", function() {
+    it("returns correct message", () => {
         const context = Log.info("FOO", "FOO", "BAR");
         assert.equal(context.$message, "FOO BAR");
     });
 
-    it("returns correct output", function() {
+    it("returns correct output", () => {
         const context = Log.info("FOO", "FOO", "BAR");
-        assert.equal(context.$output, `${this.$timestamp} | [FOO] FOO BAR`);
+        assert.equal(context.$output, `${STATE.$timestamp} | [FOO] FOO BAR`);
     });
 
-    it("returns correct arguments", function() {
+    it("returns correct arguments", () => {
         const context = Log.info(["FOO", "BAR"], "FOO", "BAR");
 
         assert.deepEqual(context.$arguments, [
@@ -300,28 +308,28 @@ describe("Context", function() {
     });
 });
 
-describe("Extensions", function() {
-    it("only allow string as a name", function() {
+describe("Extensions", () => {
+    it("only allow string as a name", () => {
         const log = Log.clone();
 
         log.addExtension("send", () => {});
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
 
         log.addExtension(["send"], () => {});
-        assert.equal(this.$console, `${this.$timestamp} | [unklogger] Argument 'name' is not a string.`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [unklogger] Argument 'name' is not a string.`);
     });
 
-    it("only accepts functions", function() {
+    it("only accepts functions", () => {
         const log = Log.clone();
 
         log.addExtension("send", () => {});
-        assert.equal(this.$console, null);
+        assert.equal(STATE.$console, null);
 
         log.addExtension("send", "FOO");
-        assert.equal(this.$console, `${this.$timestamp} | [unklogger] Argument 'fn' is not a function.`);
+        assert.equal(STATE.$console, `${STATE.$timestamp} | [unklogger] Argument 'fn' is not a function.`);
     });
 
-    it("binds the extension to the context", function() {
+    it("binds the extension to the context", () => {
         const log = Log.clone();
         log.addExtension("data", () => {});
 
@@ -330,7 +338,7 @@ describe("Extensions", function() {
         assert.isFunction(context.data);
     });
 
-    it("passes context to the extension", function() {
+    it("passes context to the extension", () => {
         const log = Log.clone();
 
         let extensionContext = null;
@@ -343,7 +351,7 @@ describe("Extensions", function() {
         assert.equal(context, extensionContext);
     });
 
-    it("passes arguments to the extension", function() {
+    it("passes arguments to the extension", () => {
         const log = Log.clone();
 
         let extensionArgs = null;
@@ -356,7 +364,7 @@ describe("Extensions", function() {
         assert.deepEqual(extensionArgs, ["FOO", "BAR"]);
     });
 
-    it("can chain extensions", function() {
+    it("can chain extensions", () => {
         const log = Log.clone();
 
         let extensionContext = 0;
